95a2a380f18b127904a051c3f247cd6bbca69293
DougFunny <douglaseduardo220483@gmail.com>
2019-11-28 16:19:40 -0300

    no commit para o norba e lucas

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Switch from './components/Switch';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
 import './App.css';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-
-  render() {
-    return (
-
+function App() {
+  return (
     <BrowserRouter>
-      <Switch />
+      <Switch>
+        <Route exact path='/' component={MovieList} />
+        <Route path='/movies/:id' component={MovieDetails} />
+        <Route path='/movies/new' component={NewMovie} />
+        <Route path='/movies/:id/edit' component={EditMovie} />
+      </Switch>
     </BrowserRouter>
-      
-    );
-  }
+  );
 }
+
+export default App;
