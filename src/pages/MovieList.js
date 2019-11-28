dc77456c9a6bfe5d5699239b73575266c73654a9
Guilherme Luiz <guiiluiz44@gmail.com>
2019-11-28 19:13:45 -0300

    configura componente MovieList.js

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
 import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      movies: [],
+    };
+  }
+
+  componentDidMount() {
+    movieAPI.getMovies()
+      .then((data) => this.setState({
+        loading: false,
+        movies: data,
+      }));
+  }
+
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
+    if (this.state.loading) return <Loading />;
 
     return (
       <div className="movie-list">
