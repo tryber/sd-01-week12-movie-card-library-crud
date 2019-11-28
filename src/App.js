5518f6b65e4a1f006195157a2f059cf69061888f
Conrado Medeiros <conradodemedeiros@hotmail.com>
2019-11-28 19:56:24 -0300

    finalizado tópico 1, 2, 3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={MovieList} />
+        <Route path="/:id" component={MovieDetails} />
+        <Route path="/new" component={NewMovie} />
+        <Route path="/:id/edit" component={EditMovie} />
+        <Route component={Notfound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
+
 
 class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      movie: [],
+      loading: true,
+     };
+  }
+
+  componentDidMount(){
+    movieAPI.getMovies()
+    .then((data) => this.setState({movie: data, loading: false}))
+  }
+
   render() {
     const { movies } = this.state;
-
     // Render Loading here if the request is still happening
 
     return (
