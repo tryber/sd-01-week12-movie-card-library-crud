2bd70557882262e9e43a6399f75f60d11eaac457
Henrique Eyer <henriqueeyer@outlook.com>
2019-11-28 16:22:39 -0300

    Tela carregando pronta

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import {
+  MovieDetails,
+  MovieList,
+  NewMovie,
+  EditMovie,
+  NotFound,
+} from './pages';
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route path="/" component={MovieList} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route component={NotFound} />
+      </Switch>
     </Router>
   );
 }
