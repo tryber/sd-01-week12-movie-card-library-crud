01d71ddbdba171d7ac14659be13ed3f7f3cd3e6e
antoniopaolillo <antonio_francisco_p@live.com>
2019-11-28 17:14:42 -0300

    criando rotas e adicionando function getMOvies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import MovieList from "./pages/MovieList";
+import MovieDetails from "./pages/MovieDetails";
+import NewMovie from "./pages/NewMovie";
+import EditMovie from "./pages/EditMovie";
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Liibrary CRUD</div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={MovieList} />
+        <Route path="/movies/:id" exact component={MovieDetails} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route component={NotFound}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
