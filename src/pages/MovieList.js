681c3955c0766bd4c5c6a3ff1b2a15de7cb49dc2
Guilherme Luiz <guiiluiz44@gmail.com>
2019-11-28 19:45:17 -0300

    adiciona link para adicionar filmes na pagina inicial

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
@@ -27,6 +28,7 @@ class MovieList extends Component {
 
     return (
       <div className="movie-list">
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
     );
