cd92edc5f7304e656b13ad20a3e87669acf38f36
Richard Wellerson <richardwellerson@gmail.com>
2019-11-28 17:32:55 -0300

    Primeira, segunda e terceira parte do projeto feitas.

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -6,9 +6,6 @@ import * as movieAPI from '../services/movieAPI';
 class MovieList extends Component {
   render() {
     const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
     return (
       <div className="movie-list">
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
