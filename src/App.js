5c5bb5665510d0f16c4f799a2fc2fdae37b2e8ae
Anderson Bolivar <bolivar.anderson@hotmail.com>
2019-11-29 17:59:39 -0200

    Render Routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
+import {MovieList} from './pages/MovieList';
+import {MovieDetails} from './pages/MovieDetails';
+import {newMovie} from './pages/NewMovie';
+import {editMovie} from './pages/EditMovie';
+import {notFound} from './pages/NotFound';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={} />
-          <Route exact path="/" component={} />
-          <Route exact path="/" component={} />
-          <Route exact path="/" component={} />
+          <Route exact path="/" component={MovieList} />
+          <Route path="/movies/:id" component={MovieDetails} />
+          <Route path="/movies/new" component={newMovie} />
+          <Route path="/movies/:id/edit" component={editMovie} />
+          <Route path="*" component={notFound} />
         </Switch>
       </BrowserRouter>
     </div>
