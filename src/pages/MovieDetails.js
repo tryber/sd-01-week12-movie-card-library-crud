f34603a349f79a14446961574fb7c678677fb0a3
DougFunny <douglaseduardo220483@gmail.com>
2019-11-30 14:24:00 -0300

    MovieDetails
    Algunas aspas duplas

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -33,16 +33,16 @@ class MovieDetails extends Component {
         <div className="col s12 m7">
           <div className="card">
             <div className="card-image">
-              <img alt='Movie Cover' src={`../${imagePath}`} />
-              <span className='card-title'>{title}</span>
+              <img alt="Movie Cover" src={`../${imagePath}`} />
+              <span className="card-title">{title}</span>
             </div>
-            <div className='card-content'>
+            <div className="card-content">
               <p>{`Subtitle: ${subtitle}`}</p>
               <p>{`Storyline: ${storyline}`}</p>
               <p>{`Genre: ${genre}`}</p>
               <p>{`Rating: ${rating}`}</p>
             </div>
-            <div className='card-action'>
+            <div className="card-action">
               <Link to={`/movies/${this.props.match.params.id}/edit`}> EDITAR </Link>
               <Link to="/"> VOLTAR </Link>
               <Link to="/" onClick={() => movieAPI.deleteMovie(this.props.match.params.id)}>
