0e873ef2c3cc1023b22b4bf50ca45bd160229454
DougFunny <douglaseduardo220483@gmail.com>
2019-11-30 14:12:41 -0300

    App
    Aspas simples trocada por aspas duplas

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' component={MovieList} />
-        <Route exact path='/movies/new' component={NewMovie} />
-        <Route exact path='/movies/:id' component={MovieDetails} />
-        <Route exact path='/movies/:id/edit' component={EditMovie} />
+        <Route exact path="/" component={MovieList} />
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+        <Route exact path="/movies/:id/edit" component={EditMovie} />
         <Route exact component={NotFound} />
       </Switch>
     </BrowserRouter>
