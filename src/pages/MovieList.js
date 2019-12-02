5ccadf0b072a950c9971f6976106125f55d46242
Richard Wellerson <richardwellerson@gmail.com>
2019-12-02 13:36:12 -0300

    Subindo alteraçoes em Movie List e NewMovie

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
@@ -27,7 +28,9 @@ class MovieList extends Component {
     return (
       <div className="movie-list">
         {movies.map((movie) => <MovieCard key={movie.title} movie={movie} />)}
+        <Link to="/movies/new">New Card</Link>
       </div>
+
     );
   }
 }
--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
-
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
+import { logicalExpression } from '@babel/types';
+import { Loading } from '../components';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      movies: [],
+      redirect: false,
+    };
   }
 
   handleSubmit(newMovie) {
-  }
-
-  render() {
-    return (
-      <MovieForm onSubmit={this.handleSubmit} />
+    movieAPI.createMovie(newMovie).then = (
+      this.setState({
+        redirect: true,
+      })
     );
   }
+
+
+  render() {
+    if (this.state.redirect) return <Loading />;
+    return <MovieForm onSubmit={this.handleSubmit} />;
+  }
 }
+
 export default NewMovie;
