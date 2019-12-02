70c61de32a5eba38c1507b00d558eb4bb7f57241
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2019-12-02 10:16:03 -0300

    9 e 10 finalizados

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import {
-  MovieList, MovieDetails, NewMovie, EditMovie, NotFound,
+  MovieList, MovieDetails, NewMovie, NotFound,
 } from './pages';
+import EditMovie from './pages/EditMovie';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={MovieList} />
-        <Route path="/movies/:id" component={MovieDetails} />
         <Route path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
         <Route path="/movies/:id/edit" component={EditMovie} />
         <Route component={NotFound} />
       </Switch>
